2ca35dd225866d4cfc6df19fcd70803f8bf717b1
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2023-02-08 14:51:09 +0530

    Create Different Components

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
     params: { hl: "en", gl: "US" },
     headers: {
-        "X-RapidAPI-Key": '26c1be505bmsh16a81b6c62b2ed1p112791jsn17bf434b65cf' ,
+        "X-RapidAPI-Key":  '26c1be505bmsh16a81b6c62b2ed1p112791jsn17bf434b65cf',
         "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
 };
