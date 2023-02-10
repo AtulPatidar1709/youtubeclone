9ea8dd02c8e55bf85ed051291713738b333ddd0d
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2023-02-10 13:43:04 +0530

    Chaanges in Comoponents Files

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,8 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
     params: { hl: "en", gl: "US" },
     headers: {
-        "X-RapidAPI-Key":  '26c1be505bmsh16a81b6c62b2ed1p112791jsn17bf434b65cf',
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
         "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
 };
@@ -14,5 +15,3 @@ export const fetchDataFromApi = async (url) => {
     const { data } = await axios.get(`${BASE_URL}/${url}`, options);
     return data;
 };
-
-// || "YOUR_API_KEY"
